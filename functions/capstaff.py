fdbd9473e1cc969edd37fcfcdb940957558f378b
HarryBrisson <ejbrisson@gmail.com>
2019-06-15 22:26:57 -0700

    add functions to get simplified color distribution

--- functions/capstaff.py
+++ functions/capstaff.py
@@ -4,3 +4,23 @@ import pandas as pd
 import numpy as np
 
 
+def get_simplified_color_distribution(frame):
+    
+    simplified_color_distribution = {}
+    
+    for row in frame:
+        
+        for pixel in row:
+            
+            simplified_color = ""
+            for color in pixel:
+                simplified_color += str(color*8//256)
+            
+            if simplified_color in simplified_color_distribution.keys():
+                simplified_color_distribution[simplified_color] += 1
+            else:
+                simplified_color_distribution[simplified_color] = 1
+                
+        return simplified_color_distribution
+    
+
